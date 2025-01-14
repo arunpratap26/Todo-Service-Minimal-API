54cc9bd603f6806414962eda14ea30e938fa29dc
arunpratap26 <arunpratap26@yahoo.com>
2025-01-15 02:44:25 +0530

    Created minimal API for Todos

---
+++ TodoMinimalApi/TodoDb.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoMinimalApi
+{
+    public class TodoDb(DbContextOptions<TodoDb> options) : DbContext(options)
+    {
+        public DbSet<TodoItem> Todos { get; set; }
+    }
+}
